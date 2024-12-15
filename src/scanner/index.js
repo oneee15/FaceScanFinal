2b332a68faf9c3066ae9d4f827c59abe0f93daba
oneee15 <johnalvin211@gmail.com>
2024-12-15 22:06:55 +0800

    upload image detection

--- src/scanner/index.js
+++ src/scanner/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Image } from "react-native";
 import { Camera, getCameraDevice } from 'react-native-vision-camera';
 import { useNavigation } from "@react-navigation/native";
 
@@ -8,6 +8,9 @@ import { postDetect } from "../utils/api";
 
 import { GradientButton } from '../components/Buttons';
 import BoundingBox from "../components/Boundingbox";
+import MakeIcon from "../components/Icons";
+import { launchImageLibrary } from "react-native-image-picker";
+import { Modal, Card, Button, Spinner } from "@ui-kitten/components";
 
 const groupByClassName = (arr) => {
     return arr.reduce((acc, item) => {
@@ -31,6 +34,9 @@ const ScannerScreen = () => {
 
     const [diseases, setDiseases] = useState([])
     const [boxes, setBoxes] = useState([])
+
+    const [uploadedImage, setUploadedImage] = useState(null)
+    const [visible, setVisible] = useState(false)
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -93,6 +99,43 @@ const ScannerScreen = () => {
 
     }
 
+    const handleUpload = async () => {
+        const result = await launchImageLibrary({
+            mediaType: 'photo'
+        })
+
+        if (result?.assets.length > 0) {
+
+            setVisible(true)
+            setDetecting(true)
+
+            let uploadedFile = {
+                ...result?.assets[0],
+                path: result?.assets[0].originalPath
+            }
+
+            setUploadedImage(uploadedFile)
+
+            const detection = await postDetect(uploadedFile)
+            const { predictions } = detection
+
+            console.log("Predictions:", predictions)
+
+            if (predictions.length > 0) {
+
+                let namedPredictions = predictions.map((pred) => {
+                    return { className: pred["class"], probability: pred["confidence"] }
+                })
+                setDiseases((prev) => [...prev, ...namedPredictions])
+                setBoxes(predictions)
+                setDetecting(false)
+            } else {
+                setDetecting(false)
+                setUploadedImage(null)
+            }
+        }
+    }
+
     const handleScanProceedBtn = () => {
         console.log("Detecting State: ", detecting)
         console.log("Detections: ", diseases)
@@ -116,36 +159,51 @@ const ScannerScreen = () => {
                     isActive={true}
                 />
             </View>
-            <View style={{ flex: 0, flexDirection: 'row', paddingHorizontal: 18, paddingVertical: 30 }}>
-                <GradientButton
-                    onPress={handleSwitchCamera}
-                    text="Switch"
-                    iconProp={{
-                        type: 'AntDesign',
-                        name: 'camera',
-                        size: 20,
-                        color: '#fff',
-                        style: { flex: 0, marginTop: 0 }
-                    }}
-                    containerStyle={{ flex: 5 }}
-                    bgColors={colors.warning.gradient}
-                />
-                <View style={{ flex: 1 }} />
-                <GradientButton
-                    onPress={handleScanProceedBtn}
-                    text={diseases?.length > 0 ? "Show Results" : detecting ? "Detecting" : "Start Scanning"}
-                    iconProp={{
-                        float: 'right',
-                        type: 'AntDesign',
-                        name: diseases?.length > 0 ? "search1" : "scan1",
-                        size: 25,
-                        color: '#fff',
-                        style: { flex: 0, marginTop: 0 }
-                    }}
-                    containerStyle={{ flex: 9 }}
-                />
+            <View style={{ flex: 0.5, alignItems: 'center' }}>
+                <Text style={{ marginTop: 5 }}>{diseases?.length > 0 ? "Show Results" : detecting ? "Detecting" : "Start Scanning"}</Text>
+            </View>
+            <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: 18, paddingVertical: 30 }}>
+
+                <View style={{ flex: 1 }}></View>
+
+                <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={handleSwitchCamera} style={{ alignItems: 'center', justifyContent: 'center' }}>
+                        <MakeIcon name="camera" type="AntDesign" size={30} />
+                        <Text>Switch</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 5, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={handleScanProceedBtn} style={{ flex: 0, width: 100, height: 100, borderRadius: 50, alignItems: "center", justifyContent: 'center', backgroundColor: colors.text.primary }}>
+                        <MakeIcon name={diseases?.length > 0 ? "search1" : "scan1"} type="AntDesign" size={45} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={handleUpload} style={{ alignItems: 'center', justifyContent: 'center' }}>
+                        <MakeIcon name="upload" type="AntDesign" size={30} />
+                        <Text>Upload</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flex: 1 }}></View>
+
             </View>
             <BoundingBox boxes={boxes} />
+
+            <Modal visible={visible}>
+                <Card disabled={true} style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: Dimensions.get("screen").width, height: Dimensions.get("screen").height }}>
+                    {uploadedImage && <Image source={{ uri: uploadedImage?.uri }} style={{ width: Dimensions.get("screen").width - 50, height: 450 }} />}
+                    {detecting && <Text style={{ textAlign: 'center', alignItems: 'center', justifyContent: 'center', paddingVertical: 15, color: '#000' }}>
+                        <Spinner /> Detecting...
+                    </Text>}
+                    {diseases && diseases.length > 0 && <Text style={{ textAlign: 'center', alignItems: 'center', justifyContent: 'center', paddingVertical: 15, color: '#000' }}>{diseases[0]?.className}</Text>}
+                    <Button disabled={detecting} onPress={handleProceed}>
+                        SHOW RESULTS
+                    </Button>
+                </Card>
+            </Modal>
+
         </View>
     )
 }
